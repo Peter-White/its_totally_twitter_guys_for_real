6e8de5c89c22acd1656fd224f6835c4f8781fcc9
Peter White <pwhitedeveloper@gmail.com>
2019-04-30 10:42:37 -0400

    inital commit base layout

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+# setup imports
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+# setup app variables
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+# go to routes
+from app import routes
